e7c56d461b4085bebbe316f7646e1de04fe3e3e1
montgomeri15 <montgomeri15@gmail.com>
2022-10-28 20:24:00 +0300

    Leetcode homework 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,16 +4,21 @@ import java.lang.reflect.Array;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
+        String[] arr = {"flower","flow","flight"};
+        System.out.println(longestCommonPrefix(arr));
     }
 
-    public static boolean isPalindrome(String s) {
-        if (s.length() == 0){
-            return false;
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0){
+            return "";
         }
-        String cleanString = s.replaceAll("\\p{Punct}|\\p{Space}", "").toLowerCase();
-        String reverseString = new StringBuilder(cleanString).reverse().toString();
-        return cleanString.equals(reverseString);
+        String pref = strs[0];
+        for (int i = 1; i < strs.length; i++){
+            while(strs[i].indexOf(pref) != 0){
+                pref = pref.substring(0, pref.length()-1);
+            }
+        }
+        return pref;
     }
 }
 
